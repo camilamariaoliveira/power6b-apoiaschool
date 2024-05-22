7d075c35286ed1a7e9d4868d1c44646859f2dfbb
camilamariaoliveira <camilamoliveiratech@gmail.com>
2024-05-22 13:21:20 -0300

    modificando botão de salvar para melhor UX

--- src/main/java/sgb/apresentacao/AtendimentoCriacaoRota.java
+++ src/main/java/sgb/apresentacao/AtendimentoCriacaoRota.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -22,6 +23,7 @@ public class AtendimentoCriacaoRota extends VerticalLayout {
         add(formulario);
 
         var salvarBotao = new Button("Salvar");
+        salvarBotao.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         salvarBotao.addClickListener(this::salvar);
 
         var cancelarBotao = new Button("Cancelar");
