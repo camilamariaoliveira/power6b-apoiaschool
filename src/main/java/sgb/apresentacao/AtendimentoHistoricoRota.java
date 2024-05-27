2e0acc5be328b87a3f424026adadecbc1fe4dfce
camilamariaoliveira <camilamoliveiratech@gmail.com>
2024-05-27 15:00:00 -0300

    fix/ dos dados mostrados no historico e redirecionamento pra editar o atenidmento escolhido

--- src/main/java/sgb/apresentacao/AtendimentoHistoricoRota.java
+++ src/main/java/sgb/apresentacao/AtendimentoHistoricoRota.java
@@ -2,6 +2,7 @@ package sgb.apresentacao;
 
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -26,23 +27,21 @@ public class AtendimentoHistoricoRota extends VerticalLayout {
         this.filtroNome = new TextField();
         var titulo = new H1("Histórico");
 
-
         add(titulo);
         filtroNome.setPlaceholder("Filtrar por nome...");
         filtroNome.addValueChangeListener(e -> atualizarLista());
-        grid.setColumns("id", "nome", "curso", "data", "periodo", "anotacoes");
-        //grid.getColumnByKey("marcadores").setHeader("Marcadores");
+        grid.setColumns("nome", "curso", "data", "periodo");
+        grid.addItemClickListener(event -> {
+            Atendimento atendimentoSelecionado = event.getItem();
+            if (atendimentoSelecionado != null) {
+                String idAtendimento = atendimentoSelecionado.getId().toString();
+                getUI().ifPresent(ui -> ui.navigate("atendimento/edicao/" + idAtendimento));
+            } else {
+                Notification.show("Nenhum atendimento selecionado");
+            }
+        });
         add(filtroNome, grid);
         atualizarLista();
-//        var atendimentos = servico.pesquisar();
-//        var grade = new Grid<Atendimento>(Atendimento.class, false);
-//        grade.addColumn("id").setHeader("Id");
-//        grade.addComponentColumn(this::edicaoAtendimento1).setHeader("Nome");
-//        grade.addComponentColumn(this::edicaoAtendimento2).setHeader("Curso");
-//        grade.addComponentColumn(this::edicaoAtendimento3).setHeader("Período");
-//
-//        grade.setItems(atendimentos);
-//        add(grade);
     }
 
     private void atualizarLista() {
@@ -54,26 +53,4 @@ public class AtendimentoHistoricoRota extends VerticalLayout {
         }
         grid.setItems(atendimentos);
     }
-
-    private RouterLink edicaoAtendimento1(Atendimento atendimento) {
-        var id = atendimento.getId();
-        var link = new RouterLink(AtendimentoEdicaoRota.class, id);
-        link.setText(atendimento.getNome());
-        return link;
-    }
-
-    private RouterLink edicaoAtendimento2(Atendimento atendimento) {
-        var id = atendimento.getId();
-        var link = new RouterLink(AtendimentoEdicaoRota.class, id);
-        link.setText(atendimento.getCurso());
-        return link;
-    }
-
-    private RouterLink edicaoAtendimento3(Atendimento atendimento) {
-        var id = atendimento.getId();
-        var link = new RouterLink(AtendimentoEdicaoRota.class, id);
-        link.setText(String.valueOf(atendimento.getPeriodo()));
-        return link;
-    }
 }
-
