c2152b7717955ff1b5ff64a1ba7b09c91ebe4e93
camilamariaoliveira <camilamoliveiratech@gmail.com>
2024-05-27 14:51:56 -0300

    fix/ excluindo código não utilizado e melhoria do código ja implementado

--- src/main/java/sgb/apresentacao/AtendimentoCriacaoRota.java
+++ src/main/java/sgb/apresentacao/AtendimentoCriacaoRota.java
@@ -1,12 +1,7 @@
 package sgb.apresentacao;
 
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,55 +17,14 @@ public class AtendimentoCriacaoRota extends VerticalLayout {
     public AtendimentoCriacaoRota() {
 
         formulario = new AtendimentoFormulario();
-        System.out.println(formulario);
-        System.out.println(this);
         add(formulario);
+
         formulario.addSaveListener(event -> {
-            System.out.println("Passou");
-            System.out.println(servico);
             Atendimento atendimento = formulario.criarAtendimento();
             servico.criarAtendimento(atendimento);
+            Notification.show("Atendimento salvo com sucesso");
+            UI.getCurrent().navigate(AtendimentoHistoricoRota.class);
         });
 
-//        var salvarBotao = new Button("Salvar");
-//        salvarBotao.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-//        salvarBotao.addClickListener(this::salvar);
-//
-//        var cancelarBotao = new Button("Cancelar");
-//        cancelarBotao.addClickListener(this::cancelar);
-//
-//        var botoes = new HorizontalLayout();
-//        botoes.add(salvarBotao);
-//        botoes.add(cancelarBotao);
-//
-//        add(botoes);
-    }
-
-    private void salvar(ClickEvent<Button> evento) {
-        try {
-            var atendimento = formulario.criarAtendimento();
-            servico.salvar(atendimento);
-
-            var atualTela = UI.getCurrent();
-            atualTela.navigate(AtendimentoHistoricoRota.class);
-        } catch (RuntimeException excecao) {
-            tratar(excecao);
-        }
-    }
-
-    private void cancelar(ClickEvent<Button> evento) {
-    }
-
-    private void tratar(RuntimeException excecao) {
-        var mensagem = excecao.getMessage();
-
-        var conteudo = "<ul><li>" + mensagem + "</li></ul>";
-        var html = new Html(conteudo);
-
-        var dialogo = new ConfirmDialog();
-        dialogo.setHeader("Erro");
-        dialogo.add(html);
-        dialogo.setConfirmText("Fechar");
-        dialogo.open();
     }
 }
