947264810217e18dc67931800561ec9404de3a1f
camilamariaoliveira <camilamoliveiratech@gmail.com>
2024-05-27 14:53:08 -0300

    fix/ da rota com melhoria do código e remoção de código não utilizado

--- src/main/java/sgb/apresentacao/AtendimentoEdicaoRota.java
+++ src/main/java/sgb/apresentacao/AtendimentoEdicaoRota.java
@@ -1,11 +1,7 @@
 package sgb.apresentacao;
 
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
@@ -13,84 +9,42 @@ import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import sgb.entidades.Atendimento;
 import sgb.negocio.AtendimentoServico;
-
 import java.util.UUID;
 
 @Route("/atendimento/edicao")
-public class AtendimentoEdicaoRota extends VerticalLayout implements HasUrlParameter<UUID> {
+public class AtendimentoEdicaoRota extends VerticalLayout implements HasUrlParameter<String> {
     @Autowired
     private AtendimentoServico servico;
-    @Autowired
     private AtendimentoFormulario formulario;
     private Atendimento atendimento;
 
     public AtendimentoEdicaoRota() {
+
         formulario = new AtendimentoFormulario();
-        formulario.preencherCampos(atendimento);
+
         add(formulario);
+
         formulario.addSaveListener(event -> {
-            Atendimento atendimento = formulario.criarAtendimento();
-            servico.criarAtendimento(atendimento);
+            try {
+                formulario.preencherAtendimento(atendimento);
+                servico.atualizarAtendimento(atendimento);
+                UI.getCurrent().navigate(AtendimentoHistoricoRota.class);
+
+            } catch (Exception e){
+                Notification.show("Erro ao atualizar atendimento: " + e.getMessage(), 3000, Notification.Position.MIDDLE);
+            }
         });
-//        var salvarBotao = new Button("Salvar");
-//        salvarBotao.addClickListener(this::salvar);
-//
-//        var excluirBotao = new Button("Excluir");
-//        excluirBotao.addClickListener(this::excluir);
-//
-//        var botoes = new HorizontalLayout();
-//        botoes.add(salvarBotao);
-//        botoes.add(excluirBotao);
-//
-//        add(botoes);
-    }
-
-
-    private void salvar(ClickEvent<Button> evento) {
-        try {
-            formulario.preencherAtendimento(atendimento);
-            servico.salvar(atendimento);
-
-            var atualTela = UI.getCurrent();
-            atualTela.navigate(AtendimentoHistoricoRota.class);
-        } catch (RuntimeException excecao) {
-            tratar(excecao);
-        }
-    }
-
-    private void excluir(ClickEvent<Button> evento) {
-        try {
-            var id = atendimento.getId();
-            servico.excluir(id);
-
-            var atualTela = UI.getCurrent();
-            atualTela.navigate(AtendimentoHistoricoRota.class);
-        } catch (RuntimeException excecao) {
-            tratar(excecao);
-        }
-    }
-
-    private void tratar(RuntimeException excecao) {
-        var mensagem = excecao.getMessage();
-
-        var conteudo = "<ul><li>" + mensagem + "</li></ul>";
-        var html = new Html(conteudo);
-
-        var dialogo = new ConfirmDialog();
-        dialogo.setHeader("Erro");
-        dialogo.add(html);
-        dialogo.setConfirmText("Fechar");
-        dialogo.open();
     }
 
     @Override
-    public void setParameter(BeforeEvent event, UUID parameter) {
-
+    public void setParameter(BeforeEvent event, String parameter) {
+        UUID id = UUID.fromString(parameter);
+        atendimento = servico.buscarAtendimentoPorId(id);
+        if (atendimento != null) {
+            formulario.preencherCampos(atendimento);
+        } else {
+            Notification.show("Atendimento não encontrado!", 3000, Notification.Position.MIDDLE);
+        }
     }
 
-    //@Override
-//    public void setParameter(BeforeEvent event, UUID parameter) {
-//            atendimento = servico.obter(parameter);
-//            formulario.preencherCampos(atendimento);
-//    }
 }
