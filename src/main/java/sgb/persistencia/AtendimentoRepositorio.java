7197b764bce78b173e24f066d0006c8a9865fcdd
camilamariaoliveira <camilamoliveiratech@gmail.com>
2024-05-27 14:48:05 -0300

    feat/ adicionando método findById

--- src/main/java/sgb/persistencia/AtendimentoRepositorio.java
+++ src/main/java/sgb/persistencia/AtendimentoRepositorio.java
@@ -11,39 +11,21 @@ import java.util.UUID;
 public class AtendimentoRepositorio {
     private List<Atendimento> atendimentos;
 
-    public AtendimentoRepositorio() {atendimentos = new ArrayList<>();
-    }
+    public AtendimentoRepositorio() {atendimentos = new ArrayList<>();}
 
     public Atendimento salvar(Atendimento atendimento) {
         atendimentos.add(atendimento);
         return atendimento;
     }
 
-    public void excluir(int id) {
-        var i = atendimentos.iterator();
-//        while (i.hasNext()) {
-//            var atendimento = i.next();
-//            if (atendimento.getId() == id) {
-//                i.remove();
-//                break;
-//            }
-//        }
-    }
-
-    public List<Atendimento> pesquisar() {
-        return new ArrayList<>(atendimentos);
-    }
-
-    public Atendimento obter(UUID id) {
-        for (var atendimento : atendimentos) {
-            if (atendimento.getId() == id) {
-                return atendimento;
-            }
-        }
-        return null;
-    }
-
     public List<Atendimento> findAll() {
         return new ArrayList<>(atendimentos);
+    }
+
+    public Atendimento findById(UUID id) {
+        return atendimentos.stream()
+                .filter(atendimento -> atendimento.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 }
