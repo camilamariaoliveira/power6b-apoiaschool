a55cb7821a67b8d3bea98a24881ad52555a945fc
camilamariaoliveira <camilamoliveiratech@gmail.com>
2024-05-27 21:05:29 -0300

    fix/ das anotações

--- src/main/java/sgb/Application.java
+++ src/main/java/sgb/Application.java
@@ -3,20 +3,11 @@ package sgb;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 
-/**
- * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
- */
 @SpringBootApplication
-//@Theme(value = "mytodo")
-@Theme(variant = Lumo.DARK)
+@Theme(value = "my-theme",variant = Lumo.DARK)
 public class Application implements AppShellConfigurator {
 
 	public static void main(String[] args) {
